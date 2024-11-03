699a5f772d6615b0dc64701106dcaeea3f5aa1af
Catalin <feiercatalin123@icloud.com>
2024-11-03 18:39:12 +0200

    FIXES

--- algDTI.py
+++ algDTI.py
@@ -1,48 +1,8 @@
 import sys
 import pygame
 import heapq
+from constants import *
 from collections import deque
-# Define colors
-WHITE = (255, 255, 255)             #for EMPTY
-BLACK = (0, 0, 0)                   #for obstacle
-BRAT = (137, 204, 4)                #for final path
-PURPLE =(105,54,156)                #for STSART
-PINKISH = (156,53,101)              #for the end
-GRAY = (90,90,90)                   #for the search
-PINK = (255,192,203)                #for the BORDER
-MENU_COLOR = (135, 37, 91)  # for menu background
-MENU_TEXT_COLOR = (195, 195, 230)  # for menu text
-HIGHLIGHT_COLOR = (180,112,176)  # for highlighted text
-# Define GRID values
-BORDER = -1
-EMPTY = 0
-OBSTACLE = 1
-START = 2
-END = 3
-PATH = 4
-SEARCH = 5
-# Define window sizes
-WIDTH = 1080
-HEIGHT = 720
-# Define The algorithm type:
-DFS = 1
-BFS = 2
-UCS = 3
-ASTAR = 4
-
-#Define the movements
-NORTH = [0, 1]
-SOUTH = [0, -1]
-WEST = [1, 0]
-EAST = [-1, 0]
-
-#Define the SONGS:
-
-DRESS = 'Dress Up Theme from Dress To Impress.mp3'
-COMP = 'Competition.mp3'
-CHARL = '365.mp3'
-RUN = 'Runway.mp3'
-SUN = 'The Last Sun.mp3'
 
 start_count=0
 end_count=0
